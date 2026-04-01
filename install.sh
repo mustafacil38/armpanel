8ced5c5ada10cbfc0713c49c6442567634fc1eb3
Mustafa <mustafacil38@gmail.com>
2026-04-01 03:16:18 +0300

    Güncelleme: 2026-04-01 03:16:18

--- install.sh
+++ install.sh
@@ -2,15 +2,26 @@
 
 # 1. Adım: Sistemi Güncelle
 echo "--- Paket listesi güncelleniyor... ---"
-apt update
-
-# 2. Adım: Paketleri Kur (-y parametresi otomatik onay verir)
-echo "--- Gerekli araçlar kuruluyor... ---"
-# apt install -y git curl wget python3 ttyd
+apt update -y
+# 2. Adım: Gerekli Paketleri Kur (-y parametresi otomatik onay verir)
+echo "--- Gerekli Paketler Kuruluyor... ---"
+apt install -y git wget python3.13 ttyd nginx
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install -y php8.4
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
+cd\
+git clone https://github.com/mustafacil38/armpanel.git
+cd/root/armpanel
+pip install -r requirements.txt --break-system-packages
 
 # 3. Adım: Kurulum Kontrolü
 echo "--- Kurulum tamamlandı. Versiyonlar kontrol ediliyor: ---"
 ttyd --version
 python3 --version
+nginx --version
+filebrowser --version
+php --version
 
 echo "--- Her şey hazır! ---"
+python3.13 app.py
