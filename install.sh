c5bfbe766db7f2d2444c3fb8c6e63abb58010887
Mustafa <mustafacil38@gmail.com>
2026-04-01 03:35:19 +0300

    Güncelleme: 2026-04-01 03:35:19

--- install.sh
+++ install.sh
@@ -1,36 +1,28 @@
-#!/bin/bash
-
-# Adım: Sistemi Güncelle
-echo "--- Paket listesi güncelleniyor... ---"
-apt update -y
-# Gerekli Paketleri Kur (-y parametresi otomatik onay verir)
-
-# Sistem paketlerini yükle
-apt update && apt install -y git wget python3.13 python3-pip ttyd nginx software-properties-common
-
-# PHP 8.4 deposunu ekle ve yükle
-add-apt-repository ppa:ondrej/php -y
+# 1. Önce temel araçları ve depo yönetim araçlarını yükle
 apt update
-apt install -y php8.4
+apt install -y curl wget git software-properties-common apt-transport-https lsb-release ca-certificates
 
-# File Browser kurulumu
+# 2. PHP 8.4 için Sury deposunu Debian'a ekle (Ubuntu PPA burada çalışmaz)
+curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+
+# 3. Python 3.13 Debian depolarında yoktur, Python 3 ve pip'i kur (Sistem sürümü kullanılır)
+apt update
+apt install -y python3 python3-pip python3-venv
+
+# 4. ttyd ve php8.4 kurulumu
+apt install -y php8.4 nginx
+# ttyd Debian depolarında yoksa binary olarak çekelim
+wget https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 -O /usr/local/bin/ttyd
+chmod +x /usr/local/bin/ttyd
+
+# 5. File Browser kurulumu
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
 
-# Proje dizinine geçiş ve kurulum
+# 6. Proje Kurulumu
 cd /root
 git clone https://github.com/mustafacil38/armpanel.git
 cd /root/armpanel
 
-# Python gereksinimlerini yükle
+# 7. Gereksinimleri yükle (Debian'da --break-system-packages kullanımı gereklidir)
 pip install -r requirements.txt --break-system-packages
-
-# 3. Adım: Kurulum Kontrolü
-echo "--- Kurulum tamamlandı. Versiyonlar kontrol ediliyor: ---"
-ttyd --version
-python3 --version
-nginx --version
-filebrowser --version
-php --version
-
-echo "--- Her şey hazır! ---"
-python3.13 app.py
